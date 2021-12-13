98e1c1eaad370ae9012300cd3478690f036ff1c4
TheStraubulous <thestraubulous@gmail.com>
2021-12-12 17:16:46 -0800

    text change test

--- App.js
+++ App.js
@@ -32,7 +32,7 @@ export default class App extends Component {
           
           <View style={styles.container}>
             <Button
-              title="imma buttin yay'"
+              title="imma buttin gittest'"
               color="#3CBBB1"
               onPress={this.playSound.bind(this)} />
           </View>
